e6249941650910382befc10d0ba5aeacc46af435
Gioele Antoci <gioele.antoci@gmail.com>
2017-03-07 20:50:27 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var config = {
+    apiKey: "AIzaSyAHVP-IXf_dMMjAPJipmiN0M4dZVmUUeVk",
+    authDomain: "polo-crawler-f920e.firebaseapp.com",
+    databaseURL: "https://polo-crawler-f920e.firebaseio.com/"
+};
+firebase.initializeApp(config);
+firebase.database().ref("polo/site").once('value').then(function (data) {
+    const obj = data.val();
+    let elements = [];
+    elements = elements.concat(Object.keys(obj).map(key => obj[key].elements));
+    const htmlElements = Object.assign({}, ...elements);
+    const tempData = [];
+    var myBarChart = new Chart(document.getElementsByClassName("chart-1-canvas")[0], {
+        type: 'horizontalBar',
+        data: {
+            labels: Object.keys(htmlElements).map(key => key),
+            datasets: [{
+                    label: "Html Element",
+                    data: Object.keys(htmlElements).map(key => htmlElements[key])
+                }]
+        },
+        options: {
+            scales: {}
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
